790062c27cb43153b17ade5a5164dfe1bd6b16b3
Stiti <https://github.com/Stiti-create>
2023-11-02 19:22:41 +0530

    report fix

--- Mimicking-Distributed-File-System/src/constants.h
+++ Mimicking-Distributed-File-System/src/constants.h
@@ -1,16 +1,16 @@
-#define N 4 // Number of clients
+#define N 3 // Number of clients
 #define L 1000 // Number of lines to receive
-#define PORT 9801 // Self port that connects to the server
+#define PORT 9803 // Self port that connects to the server
 #define PORTS 8080 // Base port for communication with other clients
 #define BUFFER_SIZE 40960 // Buffer size for receiving data
-static const char* client_ips[N] = {"10.194.23.67", "10.194.46.195",  "10.194.18.144", "10.194.8.32"}; // IP of clients
+static const char* client_ips[N] = {"10.194.17.172", "10.194.10.202", "10.194.36.225"}; // IP of clients
 // static const char* client_ips[N] = {"10.194.23.67"}; // IP of clients
-#define client_id 0 // Client id of this client
-#define submitID "cs1200397@bitmask\n" // ID used to submit the assignment
-#define serverIP "10.17.51.115"
+#define client_id 1 // Client id of this client
+#define submitID "cs1200394@bitmask\n" // ID used to submit the assignment
+#define serverIP "10.17.7.218"
 // #define DEBUG // Comment this line to disable debug messages
 // #define PLOT
-// #define DEV // This is for the SESSION RESET command that will not be avaiable in final submission
+#define DEV // This is for the SESSION RESET command that will not be avaiable in final submission
 #define RETURN(x) do \
 {                                       \
     static int a = x;                     \
